80ba3f1bbc7c68e6724248ab00a8101f35a17486
codedis213 <codedish213@gmail.com>
2015-06-30 17:55:44 +0530

    basic app with modal

---
+++ esdjhs/advisors/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=256)
+    doing_business_as_name = models.CharField(max_length=256)
+    full_address = models.CharField(max_length=256)
+
+
+class Advisor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    crd = models.IntegerKey()  # SEC unique id
+    company = models.ForeignKey(Company)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
